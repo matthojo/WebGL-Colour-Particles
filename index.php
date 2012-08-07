057643f859f65140d013b59ce78bfc8ad338e38a
Matthew Harrison-Jones <matthojo@gmail.com>
2012-08-07 14:35:19 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Particles</title>
+		<style>
+            body {
+                background-color: #000000;
+                margin: 0px;
+                overflow: hidden;
+                font-family: monospace;
+                font-size:13px;
+                text-align:center;
+                font-weight: bold;
+                text-align:center;
+            } z-index:100;
+            }
+		</style>
+	</head>
+	<body>
+		
+		<div id="container">
+
+		</div>
+
+	</body>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<script src="js/raf.js"></script>
+	<script src="js/Three.js"></script>
+	<script src="js/detector.js"></script>
+    <script src="js/stats.js"></script>
+	<script src="js/script.js"></script>
+	<script>
+    var _gaq=[['_setAccount','UA-4609610-24'],['_trackPageview']];
+    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g,s)}(document,'script'));
+  </script>
+
+<script type="text/javascript">
+  var GoSquared={};
+  GoSquared.acct = "GSN-134810-Q";
+  (function(w){
+    function gs(){
+      w._gstc_lt=+(new Date); var d=document;
+      var g = d.createElement("script"); g.type = "text/javascript"; g.async = true; g.src = "//d1l6p2sc9645hc.cloudfront.net/tracker.js";
+      var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(g, s);
+    }
+    w.addEventListener?w.addEventListener("load",gs,false):w.attachEvent("onload",gs);
+  })(window);
+</script>
+</html>
---
+++ js/script.js
@@ -0,0 +1,223 @@
+if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
+
+/******
+ *
+ * SCENE
+ *
+ *******/
+
+
+
+// set the scene size
+var WIDTH = window.innerWidth,
+    HEIGHT = window.innerHeight;
+
+// set some camera attributes
+var VIEW_ANGLE = 45,
+    ASPECT = WIDTH / HEIGHT,
+    NEAR = 0.1,
+    FAR = 10000;
+
+var scene = new THREE.Scene();
+
+
+/******
+ *
+ * RENDERER
+ *
+ *******/
+
+// create a WebGL renderer, camera
+// and a scene
+var renderer = new THREE.WebGLRenderer({ antialias: true } );
+// start the renderer
+// start the renderer - set the clear colour
+// to a full black
+renderer.setClearColor(new THREE.Color(0, 1));
+renderer.setSize(WIDTH, HEIGHT);
+
+// get the DOM element to attach to
+// - assume we've got jQuery to hand
+var $container = $('#container');
+// attach the render-supplied DOM element
+$container.append(renderer.domElement);
+
+stats = new Stats();
+stats.domElement.style.position = 'absolute';
+stats.domElement.style.top = '0px';
+$container.append( stats.domElement );
+
+/******
+ *
+ * CAMERA
+ *
+ *******/
+
+
+var camera = new THREE.PerspectiveCamera(  VIEW_ANGLE,
+    ASPECT,
+    NEAR,
+    FAR  );
+// the camera starts at 0,0,0 so pull it back
+camera.position.z = 2000;
+// and the camera
+scene.add(camera);
+
+// create a camera contol
+controls = new THREE.TrackballControls(camera);
+controls.target.set( 0, 0, 0 );
+
+controls.rotateSpeed = 1.0;
+controls.zoomSpeed = 1.2;
+controls.panSpeed = 0.8;
+
+controls.noZoom = false;
+controls.noPan = false;
+
+controls.staticMoving = false;
+controls.dynamicDampingFactor = 0.15;
+
+controls.keys = [ 65, 83, 68 ];
+
+/******
+ *
+ * CENTRAL AXIS MARKER
+ *
+ *******/
+
+var lineGeo = new THREE.Geometry();
+lineGeo.vertices.push(
+    new THREE.Vector3(-100, 0, 0), new THREE.Vector3(100, 0, 0),
+    new THREE.Vector3(0, -100, 0), new THREE.Vector3(0, 100, 0),
+    new THREE.Vector3(0, 0, -100), new THREE.Vector3(0, 0, 100)
+);
+var lineMat = new THREE.LineBasicMaterial({
+    color: 0xFFFFFF, lineWidth: 1});
+var line = new THREE.Line(lineGeo, lineMat);
+line.type = THREE.Lines;
+scene.add(line);
+
+
+
+/******
+ *
+ * Lights
+ *
+ *******/
+
+var wireMat = new THREE.MeshBasicMaterial({color: 0xFFFFFF, wireframe: true});
+var meshCube = new THREE.Mesh(
+    new THREE.CubeGeometry(1000,1000,1000, 1,1,1), // 10 segments
+    wireMat
+);
+scene.add(meshCube);
+
+/******
+ *
+ * Particles
+ *
+ *******/
+
+// create the particle variables
+var particleCount = 1800,
+    particles = new THREE.Geometry(),
+    material,
+    colors = [],
+    sprite = THREE.ImageUtils.loadTexture( "images/particle.png" );
+
+// now create the individual particles
+for(var p = 0; p < particleCount; p++) {
+
+    var vertex = new THREE.Vector3();
+    vertex.x = 1000 * Math.random() - 500;
+    vertex.y = 1000 * Math.random() - 500;
+    vertex.z = 1000 * Math.random() - 500;
+
+    vertex.velocity = new THREE.Vector3(
+        0,				// x
+        -Math.random(),	// y
+        0);
+
+    particles.vertices.push( vertex );
+
+    colors[ p ] = new THREE.Color( 0xffffff );
+    colors[ p ].setRGB( vertex.x / 255 , vertex.y / 255, vertex.z / 255 );
+}
+particles.colors = colors;
+
+material = new THREE.ParticleBasicMaterial( { size: 85, map: sprite, vertexColors: true, blending: THREE.AdditiveBlending,
+    transparent: true } );
+material.color.setHSV( 1.0, 0.2, 0.8 );
+
+// create the particle system
+var particleSystem =
+    new THREE.ParticleSystem(
+        particles,
+        material);
+
+// add it to the scene
+scene.add(particleSystem);
+
+// also update the particle system to
+// sort the particles which enables
+// the behaviour we want
+particleSystem.sortParticles = true;
+
+/******
+ *
+ * FUNCTIONS
+ *
+ *******/
+
+
+function animate(){
+    var time = Date.now() * 0.00005;
+    h = ( 360 * ( 1.0 + time ) % 360 ) / 360;
+    //material.color.setRGB(h, 1, 1 );
+
+   var rndmValue = Math.random() * .5;
+   for(var i=0;i<particleCount;i++) {
+        // get the particle
+        var particle = particles.vertices[i];
+
+        // check if we need to reset
+        if(particle.y <= -500) {
+            particle.y = 500;
+        }else if(particle.y >= 500){
+            particle.y = -500;
+        }
+
+       if(particle.x <= -500) {
+           particle.x = 500;
+       }
+       else if(particle.x >= 500){
+           particle.x = -500;
+       }
+
+       if(particle.z <= -500) {
+           particle.z = 500;
+       }else if(particle.z >= 500){
+           particle.z = -500;
+       }
+
+       // update the velocity
+       particle.velocity.y = rndmValue;
+       particle.velocity.x = rndmValue;
+       particle.velocity.z = rndmValue;
+
+       // and the position
+       particle.addSelf(particle.velocity);
+
+       // Update colours
+       colors[i].setRGB( particle.x / 255 , particle.y / 255, particle.z / 255 );
+       particle.colors = colors;
+
+    }
+
+    controls.update();
+    renderer.render(scene, camera);
+    stats.update();
+    requestAnimationFrame(animate);
+}
+
+animate();
